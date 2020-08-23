2463e39fe1f5f8bf0589b8306fbcf881b362c288
Lara Lesar <lara.m.lesar@gmail.com>
2020-08-22 21:26:54 -0500

    appjs setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+state feedback = {
+    feeling: 0,
+    understanding: 0,
+    support: 0,
+    comments: ''
+}
+
+
+const feedbackReducer =(state = feedback, action) => {
+    return state 
+}
+
+const store = createStore(
+    combineReducers({
+        feedbackReducer,
+    }),
+    applyMiddleware(logger)
+)
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
