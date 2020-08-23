387631c551678d1ffd3c9fea135b945523f0fb80
Lara Lesar <lara.m.lesar@gmail.com>
2020-08-23 01:37:39 -0500

    support understanding done

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -2,24 +2,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Support extends Component{
+  handleSupport = (event) => {
+    this.props.dispatch({type: 'SET_FEEDBACK', payload: event.target.name})
+  }
+
+  handleClick = () => {
+    this.props.history.push('/comments');
+  }
     render(){
-      
-        return{
-          
-
-        }
-
-    }
-    
+        return(
+            <div>
+            <h2>How well are you being supported?</h2>
+            <label for="support">Support (between 1 and 5):</label>
+            <input onChange={this.handleSupport} type="number" name="Support" />
+            <button onClick={this.handleClick}>NEXT!</button>
+            
+            
+            </div>
+        )
         
+            }
+            
+}
+
+const mapStateToProps = (reduxState) => {
+  return {
+    reduxState
+  }
 }
 
 
-
-const mapStateToProps = (reduxState) => {
-    return {
-      reduxState
-    }
-  }
 
 export default connect(mapStateToProps)(Support);
--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Understanding extends Component{
+  handleUnderstanding = (event) => {
+    this.props.dispatch({type: 'SET_FEEDBACK', payload: event.target.name})
+  }
+
+  handleClick = () => {
+    this.props.history.push('/support');
+  }
     render(){
-        return{
-
-        }
-
-    }
+        return(
+            <div>
+            <h2>How are you understanding the content?</h2>
+            <label for="understanding">Understanding (between 1 and 5):</label>
+            <input onChange={this.handleUnderstanding} type="number" name="Understanding" />
+            <button onClick={this.handleClick}>NEXT!</button>
+            
+            
+            </div>
+        )
     
         
 }
