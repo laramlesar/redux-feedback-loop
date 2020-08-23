da6f8a35348a2e6459ffc170777a5c1d2474200e
Lara Lesar <lara.m.lesar@gmail.com>
2020-08-22 21:56:07 -0500

    appjs fully setup components started

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { connect } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+import Feelings from '../Feelings/Feelings.js';
+import Understanding from '../Understanding/Understanding.js';
+import Support from '../Support/Support';
+import Comments from '..Comments/Comments.js';
 
 class App extends Component {
   render() {
@@ -11,9 +18,23 @@ class App extends Component {
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
+        <Route exact path="/" component={Feelings} />
+        <Route exact path="/understanding" component={Understanding} />
+        <Route exact path="/support" component={Support} />
+        <Route exact path="/comments" component={Comments} />
+        <Feelings />
+        <Understanding />
+        <Support />
+        <Comments />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (reduxState) => {
+  return {
+    reduxState
+  }
+}
+
+export default connect(mapStateToProps) App;
